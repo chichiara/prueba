0afb245f319c5fb9d22d5e803d0d3d2307f29a7a
chichiara <chichiarabenitez@gmail.com>
2025-05-07 17:18:07 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,27 @@
-window.addEventListener('DOMContentLoaded', () => {
-  const titulo = document.getElementById('titulo');
-  titulo.classList.add('mostrar');
+document.addEventListener("DOMContentLoaded", () => {
+  const aparecerItems = document.querySelectorAll(".aparecer");
+
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("visible"); 
+		observer.unobserve(entry.target);
+      }
+    });
+  }, {
+    threshold: 0.2,
+  });
+
+  aparecerItems.forEach(el => observer.observe(el));
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+    const elementos = document.querySelectorAll('.slide-in');
+
+    elementos.forEach(el => {
+    
+      setTimeout(() => {
+        el.classList.add('visible');
+      }, 500);
+    });
+  });
